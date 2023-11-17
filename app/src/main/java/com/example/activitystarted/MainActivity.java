6b902255659572043f1d0ebfb8814e581ca1510c
byemidev <emilio.arevalozuniga@gmial.com>
2023-11-17 17:24:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/activitystarted/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.activitystarted;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("www.marca.com"));
+                startActivity(i);
+            }
+        });
+    }
+}
